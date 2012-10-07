ac5c81a5efd33cf71aa00d5d3179f9a6dad6cd74
sbert <sbert.rousseau@free.fr>
2012-10-07 21:25:09 +0200

    project to test Crud for Twitter Bootstrap

---
+++ app/controllers/Posts.java
@@ -0,0 +1,6 @@
+package controllers;
+
+import play.mvc.With;
+
+public class Posts extends CRUD {    
+}
---
+++ app/models/Comment.java
@@ -0,0 +1,41 @@
+package models;
+
+import play.data.validation.MaxSize;
+import play.data.validation.Required;
+import play.db.jpa.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import java.util.Date;
+
+@Entity
+public class Comment extends Model {
+ 
+    @Required
+    public String author;
+    
+    @Required
+    public Date postedAt;
+     
+    @Lob
+    @Required
+    @MaxSize(10000)
+    public String content;
+    
+    @ManyToOne
+    @Required
+    public Post post;
+    
+    public Comment(Post post, String author, String content) {
+        this.post = post;
+        this.author = author;
+        this.content = content;
+        this.postedAt = new Date();
+    }
+    
+    public String toString() {
+        return content.length() > 50 ? content.substring(0, 50) + "..." : content;
+    }
+ 
+}
